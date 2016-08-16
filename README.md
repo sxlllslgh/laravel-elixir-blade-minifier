11025a8be30171bacf0873295549395bd0f19ee1
Ice Cream Waffle <sxlllslgh@gmail.com>
2016-08-16 20:05:25 +0800

    Use gulp-html module and update README.md.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # laravel-elixir-blade-minifier
+
+## Introduction
+This is an easy-to-use module to auto minify Laravel blade templates with laravel-elixir.
+
+## Usage
+### Simple
+Code in gulpfile.js:
+```
+var elixir = require('laravel-elixir');
+
+require('laravel-elixir-blade-minifier');
+
+elixir(function(mix) {
+    mix.blademin('original_views/**/*.blade.php', 'resources/views/');
+});
+```
+And you can use `gulp` or `gulp watch` command to minify your blade templates.
+### More
+[html-minifier](https://www.npmjs.com/package/html-minifier) options are available, but notice that blade template parser option is needed:
+```
+ignoreCustomFragments: [/@(if|elseif|for|foreach|forelse|while|continue|break).*[\r\n\s]*/,
+                            /@show/,
+                            /@stop/,
+                            /@parent/,
+                            /@endsection/,
+                            /@else/,
+                            /@endif/,
+                            /@endfor/,
+                            /@endforeach/,
+                            /@empty/,
+                            /@endforelse/,
+                            /@endwhile/,
+                            /@endpush/,
+                            /@[^(\r\n]*\([^)\r\n]*\)/
+                        ]
+```
--- index.js
+++ index.js
@@ -6,14 +6,9 @@
 
 'use strict';
 
-var BufferStreams = require('bufferstreams');
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
-var htmlmin = require('html-minifier').minify;
-var gutil = require('gulp-util');
-var objectAssign = require('object-assign');
-var Transform = require('readable-stream/transform');
-var tryit = require('tryit');
+var blademin = require('gulp-htmlmin');
 
 /*
  |--------------------------------------------------------------------------
@@ -33,6 +28,22 @@ elixir.extend('blademin', function (src, outputDir, options) {
             collapseWhitespace: true,
             minifyCss: true,
             minifyJs: true,
+            processConditionalComments: true,
+            ignoreCustomFragments: [/@(if|elseif|for|foreach|forelse|while|continue|break).*[\r\n\s]*/,
+                /@show/,
+                /@stop/,
+                /@parent/,
+                /@endsection/,
+                /@else/,
+                /@endif/,
+                /@endfor/,
+                /@endforeach/,
+                /@empty/,
+                /@endforelse/,
+                /@endwhile/,
+                /@endpush/,
+                /@[^(\r\n]*\([^)\r\n]*\)/
+            ]
         };
 
     var paths = {
@@ -42,91 +53,9 @@ elixir.extend('blademin', function (src, outputDir, options) {
 
     new elixir.Task('minify', function () {
         return gulp.src(paths.src)
-            .pipe(gulpBlademin(options))
+            .pipe(blademin(options))
             .pipe(gulp.dest(paths.outputDir))
             .pipe(new elixir.Notification('Blade templates Minified!'));
     })
         .watch(paths.src);
 });
-
-function gulpBlademin(options) {
-    return new Transform({
-        objectMode: true,
-        transform: function blademinTransform(file, enc, cb) {
-            if (file.isNull()) {
-                cb(null, file);
-                return;
-            }
-
-            function minifyBlade(buf, done) {
-                var result;
-                tryit(function() {
-                    result = new Buffer(minify(String(buf), options));
-                }, function(err) {
-                    if (err) {
-                        options = objectAssign({}, options, {fileName: file.path});
-                        done(new gutil.PluginError('gulp-htmlmin', err, options));
-                        return;
-                    }
-                    done(null, result);
-                });
-            }
-
-            function minify(data, options) {
-                options = {
-                        removeAttributeQuotes: true,
-                        removeComments: true,
-                        collapseInlineTagWhitespace: true,
-                        collapseWhitespace: true,
-                        minifyCss: true,
-                        minifyJs: true,
-                        processConditionalComments: true,
-                        ignoreCustomFragments: [/@(if|elseif|for|foreach|forelse|while|continue|break).*[\r\n\s]*/,
-                            /@show/,
-                            /@stop/,
-                            /@parent/,
-                            /@endsection/,
-                            /@else/,
-                            /@endif/,
-                            /@endfor/,
-                            /@endforeach/,
-                            /@empty/,
-                            /@endforelse/,
-                            /@endwhile/,
-                            /@endpush/,
-                            /@[^(\r\n]*\([^)\r\n]*\)/
-                        ]
-                    } || options;
-                return htmlmin(data, options);
-            }
-
-            var self = this;
-
-            if (file.isStream()) {
-                file.contents.pipe(new BufferStreams(function(none, buf, done) {
-                    minifyBlade(buf, function(err, contents) {
-                        if (err) {
-                            self.emit('error', err);
-                            done(err);
-                        } else {
-                            done(null, contents);
-                            self.push(file);
-                        }
-                        cb();
-                    });
-                }));
-                return;
-            }
-
-            minifyBlade(file.contents, function(err, contents) {
-                if (err) {
-                    self.emit('error', err);
-                } else {
-                    file.contents = contents;
-                    self.push(file);
-                }
-                cb();
-            });
-        }
-    });
-};
