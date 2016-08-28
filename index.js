1d2d2a6cd9919e63682eb3b86cdc0fcf86489fa6
Ice Cream Waffle <sxlllslgh@gmail.com>
2016-08-28 15:05:12 +0800

    Fix error options and enable custom options.

--- index.js
+++ index.js
@@ -22,29 +22,31 @@ var htmlmin = require('gulp-htmlmin');
 elixir.extend('blademin', function(src, outputDir, options) {
 
     var options = options || {
-            removeAttributeQuotes: true,
-            removeComments: true,
-            collapseInlineTagWhitespace: true,
-            collapseWhitespace: true,
-            minifyCss: true,
-            minifyJs: true,
-            processConditionalComments: true,
-            ignoreCustomFragments: [/@(if|elseif|for|foreach|forelse|while|continue|break).*[\r\n\s]*/,
-                /@show/,
-                /@stop/,
-                /@parent/,
-                /@endsection/,
-                /@else/,
-                /@endif/,
-                /@endfor/,
-                /@endforeach/,
-                /@empty/,
-                /@endforelse/,
-                /@endwhile/,
-                /@endpush/,
-                /@[^(\r\n]*\([^)\r\n]*\)/
-            ]
-        };
+        removeAttributeQuotes: true,
+        removeComments: true,
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true
+    };
+    Object.assign(options, {
+        ignoreCustomFragments: [/@(if|elseif|for|foreach|forelse|while|continue|break).*[\r\n\s]*/,
+            /@show/,
+            /@stop/,
+            /@parent/,
+            /@endsection/,
+            /@else/,
+            /@endif/,
+            /@endfor/,
+            /@endforeach/,
+            /@empty/,
+            /@endforelse/,
+            /@endwhile/,
+            /@endpush/,
+            /@[^(\r\n]*\([^)\r\n]*\)/
+        ]
+    });
 
     var paths = {
         src: src,
@@ -52,10 +54,10 @@ elixir.extend('blademin', function(src, outputDir, options) {
     };
 
     new elixir.Task('blademinify', function() {
-        return gulp.src(paths.src)
-            .pipe(htmlmin(options))
-            .pipe(gulp.dest(paths.outputDir))
-            .pipe(new elixir.Notification('Blade templates Minified!'));
-    })
+            return gulp.src(paths.src)
+                .pipe(htmlmin(options))
+                .pipe(gulp.dest(paths.outputDir))
+                .pipe(new elixir.Notification('Blade templates Minified!'));
+        })
         .watch(paths.src);
 });
